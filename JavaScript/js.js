12fabbc4b854f94b812ff94f21bc00f55511864c
Tomasz Knitter <tknitter@sii.pl>
2023-01-20 15:19:47 +0100

    layout

--- JavaScript/js.js
+++ JavaScript/js.js
@@ -1,4 +1,3 @@
-
 // var http = new XMLHttpRequest();
 // var url = "./Json/json.json";
 // http.open('get', url, true);
@@ -11,7 +10,7 @@
 //         let imageUrl = "https://picsum.photos/200/300";
 //          output += `
 //             <div class="product">
-            
+
 //                <img src="${imageUrl}" alt="${item.description}">
 //                <p class="title">${item.title}</p>
 //                <p class="description">${item.description}</p>
@@ -26,45 +25,77 @@
 //       document.querySelector(".box1").innerHTML = output;
 //    }
 // }
+let price_box = document.querySelectorAll(".box2");
+let money_box = document.querySelector(".money");
+var plus_btn = document.querySelector(".pl_btn");
+let minus_btn = document.querySelector(".min_btn");
+var all_items = document.querySelector(".total_number_of_items");
 
 
 
+//po kliknięciu w + button dodaj +1 do ilości el
+// plus_btn.addEventListener("click", function(){
+//     // var number = total_number.getAttribute("value");
+//     console.log("hello");
+// })
+
+// FETCHOWANIE DANYCH DO HTML
 fetch("./Json/json.json")
-.then( response =>  response.json()
-)
+  .then((response) => response.json())
 
-.then((products) => {
-    let placeholder  = document.querySelector(".box1");
-    let out  = "";
-    const imgUrl = "https://picsum.photos/200";
-    for(let product  of products){
-        out  += `
+  .then((products) => {
+    let placeholder = document.querySelector(".box1");
+    let out = "";
+
+    for (let product of products) {
+      
+      out += `
         <div class="product">
             
-                       <img src=${imgUrl} >
+                       <img src="${product.img}" >
                        <p class="title">${product.title}</p>
+                       <p class="manufacturer">${product.manufacturer}</p>
                        <p class="description">${product.description}</p>
-                       <p class="price">
-                          <span>${product.price}</span>
-                          <span>€</span>
-                       </p>
-                       <p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
-        
-                       <div class="money_box">
-                       <p>1</p>
-                       <p>2</p>
-                       <p>3</p>
-                       <p>4</p>
                        
+                       <div class="money_box">
+                       <div class="price"><span>${product.price}</span> <span>€</span></div>
 
+                       <div class="total_number_of_items">1</div>
+
+                       <div> 
+
+                       <button class="pl_btn">  
+                       <img class="pl" src="./assets/plus-solid.svg" alt="">
+                       </button>    
+                      
+                       <button class="min_btn"><img class="min" src="./assets/minus-solid.svg" alt=""></button>
+                      
+                       </div>
+                          
+                       
+                       <button> <img class="buy_icon" src="./assets/cart-plus-solid.svg" alt="">
+                       </button>
+                       
                        </div>
         
                        </div>
 
-        
+                        <script>
+                       plus_btn.addEventListener("click", function(){
+                        var value = all_items.innerHTML = "1"; 
+                        console.log("1")})
+                        </script>
                  `;
     }
- 
-    placeholder.innerHTML  = out;
-});
 
+    placeholder.innerHTML = out;
+
+   plus_btn.addEventListener("click", function(){
+    var value = all_items.innerHTML = "1"; 
+    console.log("1");
+   });
+
+
+  });
+
+  
